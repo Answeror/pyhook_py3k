b83670961c98c1d380970e143783f52e546fe78b
parente <parente>
2005-02-07 19:54:54 +0000

    Updated example so it only requires win32gui, not wxPython

--- example.py
+++ example.py
@@ -1,64 +1,51 @@
 import wx
 import pyHook
 
-class myFrame(wx.Frame):
-  def __init__(self):
-    wx.Frame.__init__(self, None, -1, 'My Frame')
+def OnMouseEvent(event):
+  print 'MessageName:',event.MessageName
+  print 'Message:',event.Message
+  print 'Time:',event.Time
+  print 'Window:',event.Window
+  print 'WindowName:',event.WindowName
+  print 'Position:',event.Position
+  print 'Wheel:',event.Wheel
+  print 'Injected:',event.Injected
+  print '---'
 
-    # create the hook mananger
-    self.hm = pyHook.HookManager()
-    # register two callbacks
-    self.hm.MouseAllButtonsDown = self.OnMouseEvent
-    self.hm.KeyDown = self.OnKeyboardEvent
+  # return True to pass the event to other handlers
+  # return False to stop the event from propagating
+  return True
 
-    # hook into the mouse and keyboard events
-    self.hm.HookMouse()
-    self.hm.HookKeyboard()
+def OnKeyboardEvent(event):
+  print 'MessageName:',event.MessageName
+  print 'Message:',event.Message
+  print 'Time:',event.Time
+  print 'Window:',event.Window
+  print 'WindowName:',event.WindowName
+  print 'Ascii:', event.Ascii, chr(event.Ascii)
+  print 'Key:', event.Key
+  print 'KeyID:', event.KeyID
+  print 'ScanCode:', event.ScanCode
+  print 'Extended:', event.Extended
+  print 'Injected:', event.Injected
+  print 'Alt', event.Alt
+  print 'Transition', event.Transition
+  print '---'
 
-    wx.EVT_CLOSE(self, self.OnClose)
+  # return True to pass the event to other handlers
+  # return False to stop the event from propagating
+  return True
 
-  def OnMouseEvent(self, event):
-    print 'MessageName:',event.MessageName
-    print 'Message:',event.Message
-    print 'Time:',event.Time
-    print 'Window:',event.Window
-    print 'WindowName:',event.WindowName
-    print 'Position:',event.Position
-    print 'Wheel:',event.Wheel
-    print 'Injected:',event.Injected
-    print '---'
+# create the hook mananger
+hm = pyHook.HookManager()
+# register two callbacks
+hm.MouseAllButtonsDown = OnMouseEvent
+hm.KeyDown = OnKeyboardEvent
 
-    # return True to pass the event to other handlers
-    # return False to stop the event from propagating
-    return True
-
-  def OnKeyboardEvent(self, event):
-    print 'MessageName:',event.MessageName
-    print 'Message:',event.Message
-    print 'Time:',event.Time
-    print 'Window:',event.Window
-    print 'WindowName:',event.WindowName
-    print 'Ascii:', event.Ascii, chr(event.Ascii)
-    print 'Key:', event.Key
-    print 'KeyID:', event.KeyID
-    print 'ScanCode:', event.ScanCode
-    print 'Extended:', event.Extended
-    print 'Injected:', event.Injected
-    print 'Alt', event.Alt
-    print 'Transition', event.Transition
-    print '---'
-
-    # return True to pass the event to other handlers
-    # return False to stop the event from propagating
-    return True
-
-  def OnClose(self, event):
-    del self.hm
-    self.Destroy()
+# hook into the mouse and keyboard events
+hm.HookMouse()
+hm.HookKeyboard()
 
 if __name__ == '__main__':
-  app = wx.PySimpleApp(0)
-  frame = myFrame()
-  app.SetTopWindow(frame)
-  frame.Show()
-  app.MainLoop()
+  import win32gui
+  win32gui.PumpMessages()
